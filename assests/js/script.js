4f36e20be3e5f899cfec920b382bf5a39c353480
Hollusam <oluyomisamuels@gmail.com>
2022-12-10 23:03:50 +0000

    Function to determine past/present/future time created

--- assests/js/script.js
+++ assests/js/script.js
@@ -17,3 +17,33 @@ $(document).ready(function () {
   var hours = today.hours();
     //console.log(hours);
 });
+
+// Variables for time/parent/current hour set
+var parent = $(this).parents(".row");
+var timeId = parseInt(parent.attr("id"));
+var currentHour = parseInt(moment().format("H"));
+
+//Current hour for color coding
+var index =  0;
+
+// Function to determine past/present/future time
+timeBlock.each(function () {
+    var parent = $(this).parents(".row");
+    var timeId = parseInt(parent.attr("id"));
+    var currentHour = parseInt(moment().format("H"));
+  //   console.log(timeId);
+  
+    if (timeId < currentHour) {
+      $(this).addClass("past");
+    } else if (timeId === currentHour) {
+      $(this).removeClass("past");
+      $(this).addClass("present");
+    } else {
+      $(this).removeClass("past");
+      $(this).removeClass("present");
+      $(this).addClass("future");
+  
+      $(this).val(localStorage.getItem(parent.attr("id")));
+    }
+    $(this).val(localStorage.getItem(parent.attr("id")));
+  });
